60ef973665ec0eeae59a0a0c204460f9248de277
thihasM <thihas2004@gmail.com>
2025-03-13 08:24:20 +0530

    finished about and services

---
+++ js/aboutscript.js
@@ -0,0 +1,9 @@
+<iframe 
+    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12345.678901234567!2d-122.12345678901234!3d37.123456789012345!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMzfCsDA3JzI0LjQiTiAxMjLCsDA3JzI0LjQiVw!5e0!3m2!1sen!2sus!4v1234567890123!5m2!1sen!2sus" 
+    width="100%" 
+    height="400" 
+    style="border:0;" 
+    allowfullscreen="" 
+    loading="lazy"
+    aria-label="SSCH Care Location on Google Maps">
+</iframe>
